932197a30819cf72aed43932a6ec84197d9e8138
Martin Fernandez <martin.fernandez@eclypsium.com>
2022-05-27 16:01:04 -0300

    e820 tests

---
+++ tools/testing/kunit/qemu_configs/x86_64_efi.py
@@ -0,0 +1,11 @@
+from ..qemu_config import QemuArchParams
+
+QEMU_ARCH = QemuArchParams(linux_arch='x86_64',
+			   kconfig='''
+CONFIG_SERIAL_8250=y
+CONFIG_SERIAL_8250_CONSOLE=y
+CONFIG_EFI=y''',
+			   qemu_arch='x86_64',
+			   kernel_path='arch/x86/boot/bzImage',
+			   kernel_command_line='console=ttyS0',
+			   extra_qemu_params=['-bios', '/usr/share/qemu/OVMF.fd'])
